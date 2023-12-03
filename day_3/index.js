a48ce872b99ffc085a581870ff2725b6aec171a1
NighIsTheEnd <jonathan.c.evey@gmail.com>
2023-12-03 11:37:20 -0800

    functions added to get index pair for symbols, and objects for each number

--- day_3/index.js
+++ day_3/index.js
@@ -6,4 +6,48 @@ fs.readFile("./input.txt", "utf8", (error, data) => {
     return;
   }
   let array = data.split("\n").map((e) => e.trim().split(" "));
+  partOne(array);
 });
+
+const partOne = (array) => {
+  let symbolIndexArray = [];
+  array.forEach((line, lineIndex) => {
+    line.forEach((string) => {
+      string.split("").forEach((letter, columnIndex) => {
+        if (letter.match(/[^\w.]/)) {
+          symbolIndexArray.push([lineIndex, columnIndex]);
+        }
+      });
+    });
+  });
+  console.log(symbolIndexArray);
+  let numberObjArray = [];
+  array.forEach((line, lineIndex) => {
+    console.log(line);
+    let numberObj = {
+      value: 0,
+      lineIndex: 0,
+      columnIndexArray: [],
+    };
+    line.forEach((string) => {
+      string.split("").forEach((letter, columnIndex) => {
+        if (letter.match(/[.]/) && numberObj["value"] > 0) {
+          console.log(numberObj);
+          let cloneNumberOjb = { ...numberObj };
+          numberObjArray.push(cloneNumberOjb);
+          numberObj.value = 0;
+          numberObj.lineIndex = 0;
+          numberObj.columnIndexArray = [];
+        }
+        if (letter.match(/[0-9]/)) {
+          numberObj.value = numberObj.value + Number(letter);
+          if (numberObj.lineIndex === 0) {
+            numberObj.lineIndex = lineIndex;
+          }
+          numberObj.columnIndexArray.push(columnIndex);
+        }
+      });
+    });
+  });
+  console.log(numberObjArray);
+};
